58f66bd766a51b312841ce16985648ed38fbd957
mrgao <gaogangwork@qq.com>
2019-11-14 19:33:14 +0800

    Update nav.js

--- docs/.vuepress/links/nav.js
+++ docs/.vuepress/links/nav.js
@@ -43,7 +43,7 @@ module.exports = [{
             link: "https://github.com/MrGaoGang/luckly_popup_window"
         }, {
             text: "Vue开发Mendix组件脚手架",
-            link: "https://github.com/MrGaoGang/mendix_vue_template"
+            link: "https://github.com/MrGaoGang/lucky_tools/blob/master/packages/mendix-cli/README.md"
         }]
     },
     {
